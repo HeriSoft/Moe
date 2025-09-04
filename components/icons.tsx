89859fb00aefcca6e8316558124ad3d1bb5a0064
Heri Soft <99360776+HeriSoft@users.noreply.github.com>
2025-09-05 02:10:27 +0700

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -228,7 +228,7 @@ export const AcademicCapIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) 
 
 export const ArrowPathIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0011.667 0l3.181-3.183m-11.667 0l3.181-3.183a8.25 8.25 0 00-11.667 0l-3.181 3.183" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
   </svg>
 );
 
