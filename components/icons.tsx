ed79b625b35fb6538a01ce142dae0f63049404b6
Heri Soft <99360776+HeriSoft@users.noreply.github.com>
2025-09-10 22:11:04 +0700

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -414,6 +414,12 @@ export const StopIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   </svg>
 );
 
+export const MinusIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15" />
+  </svg>
+);
+
 
 // --- ALIASES for easier import ---
 export const GearIcon = CogIcon;
