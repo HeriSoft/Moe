9fd409256ca6c853d3afaf531c9b5d1c716a95b7
Heri Soft <99360776+HeriSoft@users.noreply.github.com>
2025-09-25 06:28:31 +0700

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -514,6 +514,26 @@ export const ArrowUturnLeftIcon: React.FC<React.SVGProps<SVGSVGElement>> = (prop
   </svg>
 );
 
+// --- NEW ICONS for Pixshop ---
+export const AdjustmentsVerticalIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />
+    </svg>
+);
+
+export const PaintBrushIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9.53 16.122a3 3 0 0 0-5.78 1.128 2.25 2.25 0 0 1-2.4 2.245 4.5 4.5 0 0 0 8.4-2.245c0-.399-.078-.78-.22-1.128Zm0 0a15.998 15.998 0 0 0 3.388-1.62m-5.043-.025a15.998 15.998 0 0 1 1.622-3.385m5.043.025a15.998 15.998 0 0 0 1.622-3.385m3.388 1.62a15.998 15.998 0 0 0-1.62-3.385m0 0a4.5 4.5 0 0 0-6.364-6.364l-1.622 3.385a15.998 15.998 0 0 1-1.622 3.385m6.364 6.364a4.5 4.5 0 0 0 6.364 6.364l1.622-3.385a15.998 15.998 0 0 1 1.622-3.385" />
+    </svg>
+);
+
+export const CropIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 3.75H6A2.25 2.25 0 0 0 3.75 6v1.5M16.5 3.75H18A2.25 2.25 0 0 1 20.25 6v1.5m0 9V18A2.25 2.25 0 0 1 18 20.25h-1.5m-9 0H6A2.25 2.25 0 0 1 3.75 18v-1.5" />
+    </svg>
+);
+
+
 
 // --- ALIASES for easier import ---
 export const GearIcon = CogIcon;
