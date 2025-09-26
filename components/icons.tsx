10905791f6b27daf88379acab07b4e13bba0b962
Heri Soft <99360776+HeriSoft@users.noreply.github.com>
2025-09-26 12:59:09 +0700

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -541,6 +541,12 @@ export const EraserIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     </svg>
 );
 
+export const TypeIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 4.5v15m12-15v15M3 4.5h18" />
+    </svg>
+);
+
 
 // --- ALIASES for easier import ---
 export const GearIcon = CogIcon;
